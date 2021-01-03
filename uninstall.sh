9e063659a82f4fde8f0b12606db4d100c7222fe8
Kaustubh Kulkarni <kaustubh@pop-os.localdomain>
2021-01-03 16:31:02 +0530

    Added uninstallation script

---
+++ uninstall.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+if [[ ! -f "/usr/local/bin/envmerger" ]]
+then
+	echo "No installation found."
+else
+	sudo rm /usr/local/bin/envmerger
+
+	shell=`basename $SHELL`
+	shell_config=`echo "/home/$USER/."$shell"rc"`
+
+	if [[ -f $shell_config ]]
+	then
+		sed -i 's/alias envmerger=.*//' $shell_config
+	else
+		echo -e "Make sure to remove the alias in your ~/.`basename $SHELL`rc file!"
+	fi
+
+	echo "Succesfully uninstalled."	
+fi

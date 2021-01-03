c09c97144d8f1e8f70478ec820fcb17070635c75
Kaustubh Kulkarni <kaustubh@pop-os.localdomain>
2021-01-03 15:24:34 +0530

    Changed exit to return. Added help option

--- src/envmerger.sh
+++ src/envmerger.sh
@@ -1,44 +1,73 @@
 #!/usr/bin/bash
 
-check_absolute_path() {
-	echo "Looking for the environments to be added..."
+helper() {
+	if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]
+	then
+		echo "Help"
+		valid=0
+	fi
+}
 
+check_absolute_path() {
 	for env in "$@"
 	do
-		if [ ${env:0:1} != '/' ]
+		if [[ ${env:0:1} != '/' ]]
 		then
-			echo "$env ====> not an absolute path"
-			echo "Provide only absolute paths to your environment."
-			exit
+			echo "$env ====> not an absolute path. Provide only absolute paths to your environment."
+			valid=0
+			return
 		fi
 	done
+	valid=1
 }
 
 check_valid_envs() {
 	for env in "$@"
 	do
-		if [ ! -d $env ] || [ ! -f "$env/bin/activate" ]
+		if [[ ! -d $env ]] || [[ ! -f "$env/bin/activate" ]]
 		then
 			echo "$env ====> does not exist/is not a virtual environment"
-			exit
+			valid=0
+			return
 		fi
 	done
-
-	echo "Found environments!"
+	valid=1
 }
 
 update_python_path() {
-	echo "Adding packages..."
-
 	for env in "$@"
 	do
 		python_version=`ls "$env/lib" | grep -i python3.`
-		export PYTHONPATH="$env/lib/$python_version/site-packages:$PYTHONPATH"
+		# export PYTHONPATH="$env/lib/$python_version/site-packages:$PYTHONPATH"
 		echo -e "Added `basename $env` -> $python_version packages to current venv"
 	done
 }
 
-echo "You are in virtual environment ====> `basename $VIRTUAL_ENV`"
-check_absolute_path $@
-check_valid_envs $@
-update_python_path $@
+helper $@
+
+if [[ $valid == 1 ]]
+then
+	echo "You are in virtual environment ====> `basename $VIRTUAL_ENV`"
+	echo "Looking for the environments to be added..."
+	
+	check_absolute_path $@
+else
+	return
+fi
+
+if [[ $valid == 1 ]]
+then
+	check_valid_envs $@
+else
+	return
+fi
+
+if [[ $valid == 1 ]]
+then
+	echo "Found environments!"
+	echo "Adding packages..."
+
+	update_python_path $@
+else
+	return
+fi
